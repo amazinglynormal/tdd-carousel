dceff62604d1401c976b85067468cebfd72696fb
Stephen <stephenbrady365@gmail.com>
2021-04-09 20:02:47 +0100

    Add basic implementation od carousel

---
+++ src/tests/Carousel.test.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Carousel from "../Carousel";
+import CarouselButton from "../CarouselButton";
+import CarouselSlide from "../CarouselSlide";
+
+describe("carousel", () => {
+  let wrapper;
+
+  const slide = [
+    {
+      imgUrl: "https://example.com/slide1.png",
+      description: "Slide 1",
+      attribution: "Uno Pizzeria",
+    },
+    {
+      imgUrl: "https://example.com/slide2.png",
+      description: "Slide 2",
+      attribution: "Dos Equis",
+    },
+    {
+      imgUrl: "https://example.com/slide3.png",
+      description: "Slide 3",
+      attribution: "Three Amigos",
+    },
+  ];
+
+  beforeEach(() => {
+    wrapper = shallow(<Carousel slide={slides} />);
+  });
+
+  it("renders a <div>", () => {
+    expect(wrapper.type()).toBe("div");
+  });
+
+  it("has an initial 'slideIndex' of 0", () => {
+    expect(wrapper.state("slideIndex")).toBe(0);
+  });
+
+  it("renders a carouselButton labeled 'Prev'", () => {
+    expect(wrapper.find(CarouselButton).at(0).prop("children")).toBe("Prev");
+  });
+
+  it("renders a carouselButton labeled 'Next'", () => {
+    expect(wrapper.find(CarouselButton).at(1).prop("children")).toBe("Next");
+  });
+
+  describe("witrh a middle slide selected", () => {
+    beforeEach(() => {
+      wrapper.setState({ slideIndex: 1 });
+    });
+
+    it("decrements 'slideIndex' when Prev is clicked", () => {
+      wrapper.find("[data-action='prev']").simulate("click");
+      expect(wrapper.state("sideIndex")).toBe(0);
+    });
+
+    it("increments 'slideIndex' when Next is clicked", () => {
+      wrapper.find("[data-action='next']").simulate("click");
+      expect(wrapper.state("sideIndex")).toBe(2);
+    });
+  });
+
+  describe("with the first slide selected", () => {
+    it("wraps 'slideIndex' to the max value when Prev is clicked", () => {
+      wrapper.setState({ slideIndex: 0 });
+      wrapper.find("[data-action='prev']").simulate("click");
+      expect(wrapper.state("slideIndex")).toBe(slide.length - 1);
+    });
+  });
+
+  describe("with the last slide selected", () => {
+    it("wraps 'slideIndex' to the min value when Next is clicked", () => {
+      wrapper.setState({ slideIndex: slides.length - 1 });
+      wrapper.find("[data-action='next']").simulate("click");
+      expect(wrapper.state("slideIndex")).toBe(0);
+    });
+  });
+
+  it("renders the current slide as a CarouselSlide", () => {
+    let slideProps = wrapper.find(CarouselSlide).props();
+    expect(slideProps).toEqual(slide[0]);
+    wrapper.setState({ slideIndex: 1 });
+    (slideProps = wrapper.find(CarouselSlide)), props();
+    expect(slideProps).toEqual(slide[1]);
+  });
+});
