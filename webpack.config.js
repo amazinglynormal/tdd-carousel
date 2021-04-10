df85eb5a2565f1eef9546116a297f03e4ebaacce
Stephen <stephenbrady365@gmail.com>
2021-04-10 09:09:13 +0100

    Add example page

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+const HtmlWebapckPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: {
     carousel: "./src/Carousel.js",
+    example: "./example/index.js",
   },
+  plugins: [
+    new HtmlWebapckPlugin({ title: "Carousel Example", chunks: ["example"] }),
+  ],
   module: {
     rules: [
       {
